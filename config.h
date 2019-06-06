48b4bcfda7b7d01dac78122f0fa81787afabe3d0
Josh <joshua.hansen188@gmail.com>
2019-06-07 07:41:58 +1000

    updated default config

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -92,7 +92,7 @@ static const char *colorname[] = {
 	"red3",
 	"green3",
 	"yellow3",
-	"blue",
+	"blue2",
 	"magenta3",
 	"cyan3",
 	"gray90",
